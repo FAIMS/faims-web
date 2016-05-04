dc9acb2ce48cc955626ac78eafa3ed5fbaa3e927
Wes Cilldhaire <anomaly256@gmail.com>
2016-05-04 19:05:01 +1000

    Prevent duplicate loading of JS assets in development environment - was causing exporters to be run twice

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,6 +37,6 @@ FaimsWeb::Application.configure do
   config.assets.compress = false
 
   # Expands the lines which load the assets
-  config.assets.debug = true
+  config.assets.debug = false
 
 end
