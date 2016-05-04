d2f61f2e3234b5522e6475a8d079e132b5c3a12f
Wes Cilldhaire <anomaly256@gmail.com>
2016-05-04 17:08:41 +1000

    Mass assign known fields during backgroundjob creation

--- app/models/background_job.rb
+++ app/models/background_job.rb
@@ -4,7 +4,7 @@ class BackgroundJob < ActiveRecord::Base
   belongs_to :delayed_job, :class_name => '::Delayed::Job'
   belongs_to :user
   has_one :project_module_exports
-  attr_accessible :job_type, :module_name, :status
+  attr_accessible :job_type, :module_name, :status, :project_module, :delayed_job
   default_scope order: 'updated_at DESC'
   validates_presence_of :job_type, :module_name, :status
 end
