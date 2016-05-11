ad7ca4d3065909ed5b83e69c8680aec7ed350ab8
Andrew Foster <andy@sol1.com.au>
2016-05-11 18:23:38 +1000

    Spec for new ProjectModuleExport model

--- spec/models/project_module_export_spec.rb
+++ spec/models/project_module_export_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
 describe ProjectModuleExport do
-  pending "add some examples to (or delete) #{__FILE__}"
+	describe "Assocations" do
+		it { should belong_to(:project_module) }
+		it { should belong_to(:background_job) }
+		it { should have_many(:project_module_export_options) }
+	end
 end
