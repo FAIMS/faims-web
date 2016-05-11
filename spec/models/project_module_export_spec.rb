14b1d2efe3f60a007cb1d84853aefe25b442d033
Andrew Foster <andy@sol1.com.au>
2016-05-11 19:48:39 +1000

    Remove unused join

--- spec/models/project_module_export_spec.rb
+++ spec/models/project_module_export_spec.rb
@@ -4,6 +4,5 @@ describe ProjectModuleExport do
 	describe "Assocations" do
 		it { should belong_to(:project_module) }
 		it { should belong_to(:background_job) }
-		it { should have_many(:project_module_export_options) }
 	end
 end
