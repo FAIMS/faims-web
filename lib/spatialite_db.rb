6ca88ae4b494baf1d989b344c60002fd66d8211f
Wes Cilldhaire <anomaly256@gmail.com>
2016-07-11 16:46:09 +1000

    Initial 16.04 related changes

--- lib/spatialite_db.rb
+++ lib/spatialite_db.rb
@@ -71,6 +71,10 @@ class SpatialiteDB
 
   def self.spatialite_library
     return 'libspatialite.dylib' if (/darwin/ =~ RUBY_PLATFORM) != nil
-    return 'libspatialite.so'
+    if `. /etc/lsb-release; echo $DISTRIB_CODENAME`.strip == 'trusty'
+      return 'libspatialite.so'
+    else
+      return 'mod_spatialite.so'
+    end
   end
 end
--- puppet/modules/imagemagick/manifests/init.pp
+++ puppet/modules/imagemagick/manifests/init.pp
@@ -1,29 +1,45 @@
 class imagemagick {
-  require common
+	require common
 
-  apt::ppa { 'ppa:mc3man/trusty-media':
-  }
+	if $::lsbdistcodename == 'trusty' {
+		apt::ppa { 'ppa:mc3man/trusty-media':
+		}
 
-  exec { "update imagemagick sources":
-    path    => "/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin",
-    command => '/bin/true',
-    unless  => 'apt-get update',
-    timeout => 600,
-    require => Apt::Ppa['ppa:mc3man/trusty-media']
-  }
+		exec { "update imagemagick sources":
+			path => "/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin",
+			command => '/bin/true',
+			unless => 'apt-get update',
+			timeout => 600,
+			require => Apt::Ppa['ppa:mc3man/trusty-media']
+		}
 
-  $imagemagick_packages = ["imagemagick","libmagickwand-dev","ffmpeg","libmagickcore5-extra","ghostscript","netpbm","autotrace","html2ps","ufraw-batch","dcraw","transfig","libbz2-1.0"]
-  package { $imagemagick_packages:
-    ensure  => "present",
-    require => Exec["update imagemagick sources"]
-  }
+		$imagemagick_packages = ["imagemagick","libmagickwand-dev","ffmpeg","libmagickcore5-extra","ghostscript","netpbm","autotrace","html2ps","ufraw-batch","dcraw","transfig","libbz2-1.0"]
 
-  file { "/etc/ImageMagick/policy.xml":
-    mode    => "0644",
-    owner   => root,
-    group   => root,
-    content => template("imagemagick/policy.xml"),
-    require => Package[$imagemagick_packages]
-  }
+		package { $imagemagick_packages:
+			ensure => "present",
+			require => Exec["update imagemagick sources"]
+		}
 
+		file { "/etc/ImageMagick/policy.xml":
+			mode => "0644",
+			owner => root,
+			group => root,
+			content => template("imagemagick/policy.xml"),
+			require => Package[$imagemagick_packages]
+		}
+
+	} elsif $::lsbdistcodename == 'xenial' {
+		$imagemagick_packages = ["imagemagick","libmagickwand-dev","ffmpeg","libmagickcore-6.q16-2-extra","ghostscript","netpbm","autotrace","html2ps","ufraw-batch","dcraw","transfig","libbz2-1.0"]
+		exec { "update imagemagick sources":
+			path=> "/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin",
+			command => '/bin/true',
+			unless=> 'apt-get update',
+			timeout => 600
+		}
+
+		package { $imagemagick_packages:
+			ensure => "present",
+			require => Exec["update imagemagick sources"]
+		}
+	}
 }
