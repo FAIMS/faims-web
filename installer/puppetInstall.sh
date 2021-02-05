949bb6bf570ddc1f026fc4eff03e0127c3fc18db
Brian Ballsun-Stanton <brian@fedarch.org>
2021-02-05 12:22:16 +1100

    debugging instrumentation

--- installer/puppetInstall.sh
+++ installer/puppetInstall.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 if lsb_release -rs | grep -q 18.04 ; then 
-    echo "You're running 18.04, continuing... "
+    echo -e "\e[32mYou're running 18.04, continuing... \e[0m"
 else
     echo "FAIMS 2.6 needs to be installed only on 18.04."
     exit 1
@@ -22,52 +22,56 @@ sudo apt-get update
 # Install common packages
 sudo apt-get -y install git puppet=5.4.\* libreadline-dev software-properties-common
 
-echo "* Installing puppet modules"
+echo -e "\e[34m* Installing puppet modules\e[0m"
 # Install puppet modules
 if [ ! -d "$HOME/.puppet/modules/stdlib" ]; then
-    echo -e "\tstdlib"
+    echo -e "\e[34m\tstdlib\e[0m"
     puppet module --modulepath=$HOME/.puppet/modules/ install puppetlabs-stdlib
 fi
 
 if [ ! -d "$HOME/.puppet/modules/apt" ]; then
-    echo -e "\tapt"
+    echo -e "\e[34m\tapt\e[0m"
     puppet module --modulepath=$HOME/.puppet/modules/ install puppetlabs-apt
 fi
 
 if [ ! -d "$HOME/.puppet/modules/vcsrepo" ]; then
-    echo -e "\tvcsrepo"
+    echo -e "\e[34m\tvcsrepo\e[0m"
     puppet module --modulepath=$HOME/.puppet/modules/ install puppetlabs-vcsrepo
 fi
 
 # Clone webapp
 if [ ! -d "$APP_ROOT" ]; then
-    echo "* Cloning"
+    echo -e "\e[34m* Cloning\e[0m"
     sudo git clone https://github.com/FAIMS/faims-web.git -b $BRANCH $APP_ROOT
     sudo chown -R $USER:$USER $APP_ROOT
     cd $APP_ROOT && git checkout $BRANCH
 fi
 
 if [ ! -h "/etc/puppet/hiera.yaml" ]; then
-    echo "* Copying hiera"
+    echo -e "\e[34m* Copying hiera\e[0m"
     sudo rm -f /etc/puppet/hiera.yaml
     sudo ln -s $APP_ROOT/puppet/hiera.yaml /etc/puppet/hiera.yaml
 fi
 
-echo "* Sedding"
+echo -e "\e[34mInstalling FAIMS as user:\e[0m"
+
+ sudo puppet lookup --modulepath=$APP_ROOT/puppet/modules:$HOME/.puppet/modules --explain common::webapp_user
+
+echo -e "\e[34m* Sedding\e[0m"
 # Configure puppet
 sed -i "s/webapp_user:.*/webapp_user: $USER/g" $APP_ROOT/puppet/data/common.yaml
 sed -i "s/app_tag:.*/app_tag: production/g" $APP_ROOT/puppet/data/common.yaml
 
 # Update repo
-echo "* updating repo"
+echo -e "\e[34m* updating repo\e[0m"
 sudo puppet apply --verbose $APP_ROOT/puppet/repo.pp --modulepath=$APP_ROOT/puppet/modules:$HOME/.puppet/modules
 
 # Update server
-echo "* updating server"
+echo -e "\e[34m* updating server\e[0m"
 sudo puppet apply --verbose $APP_ROOT/puppet/update.pp --modulepath=$APP_ROOT/puppet/modules:$HOME/.puppet/modules
 
 # Test for and patch ImageTragic
-echo "* ImageTragic"
+echo -e "\e[34m* ImageTragic\e[0m"
 pushd $APP_ROOT/tools/imagemagick-poc
 ./test.sh
 case $? in
@@ -85,7 +89,7 @@ esac
 popd
 
 # Restart services
-echo "* restarting services"
+echo -e "\e[34m* restarting services\e[0m"
 sudo puppet apply $APP_ROOT/puppet/restart.pp --modulepath=$APP_ROOT/puppet/modules:$HOME/.puppet/modules
 
-echo "*** FAIMS 2.6.4 Server install completed. ***"
+echo -e "\e[34m*** FAIMS 2.6.4 Server install completed. ***\e[0m"
--- puppet/modules/repo/manifests/init.pp
+++ puppet/modules/repo/manifests/init.pp
@@ -1,11 +1,11 @@
 class repo {
   require common
 
-  $webapp_user = lookup("webapp_user")
-  $app_root = lookup("app_root")
-  $app_source = lookup("app_source")
+  $webapp_user = lookup("common::webapp_user")
+  $app_root = lookup("common::app_root")
+  $app_source = lookup("common::app_source")
   $exec_path = "/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin"
-
+  notify { 'webapp_user: $webapp_user, app_root: $app_root, app_source: $app_source', withpath => true}
   if $app_tag {
     vcsrepo { $app_root:
       ensure   => latest,
